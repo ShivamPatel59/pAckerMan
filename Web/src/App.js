8c646bf61fbc3e9c11b6998d764a18c054052a31
Shivam Patel <shivam.patel222653@gmail.com>
2023-08-13 12:23:04 +0530

    frontend deploy

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,25 +1,25 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import "./App.css"
-import Frontcard from "./components/Frontcard"
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Frontcard from "./components/Frontcard";
 
 export default function App() {
-    return (
-      <div className="App">
-        <div className="card">
-          <Frontcard
-            name="ADD ITEMS"
-            des="Add Items to new Container!"
-            img="/download.png"
-            link="http://localhost:3000/operation"
-          />
-          <Frontcard
-            name="HISTORY"
-            des="The history of packages shipped!."
-            img="/download_2.png"
-            link="http://localhost:3000/history"
-          />
-        </div>
+  return (
+    <div className="App">
+      <div className="card">
+        <Frontcard
+          name="ADD ITEMS"
+          des="Add Items to new Container!"
+          img="/download.png"
+          link="http://packerman.vercel.app//operation"
+        />
+        <Frontcard
+          name="HISTORY"
+          des="The history of packages shipped!."
+          img="/download_2.png"
+          link="http://packerman.vercel.app//history"
+        />
       </div>
-    );
+    </div>
+  );
 }
